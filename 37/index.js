7a538ae3f91b14364cc4a763372f17a7a3c625a8
KrishnanshVasaniya <119565702+KrishnanshVasaniya@users.noreply.github.com>
2025-02-24 20:03:19 +0530

    Add files via upload

---
+++ 37/index.js
@@ -0,0 +1,130 @@
+// let day = 2;
+
+// switch (day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     default:
+//         console.log("Invalid day");
+// }
+
+// for 1 to 5 numbers
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+// }
+
+// for 10 to 1 
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i);
+// }
+
+// for even numbers  
+// let i = 2;
+// while (i <= 10) {
+//     console.log(i);
+//     i += 2;
+// }
+
+// for odd numbers
+// let i = 1;
+// while (i <= 10) {
+//     console.log(i);
+//     i += 2;
+// }
+
+// Print a number at least once
+// let num = 5;
+// do {
+//     console.log(num);
+//     num--;
+// } while (num > 0);
+
+
+// let number = prompt("Enter a number:"); 
+// number = parseInt(number);
+// if (number % 2 === 0) {
+//     console.log("Even");
+// } else {
+//     console.log("Odd");
+// }
+
+// function greet() {
+//     console.log("Hello, Krishnansh!");
+// }
+
+// // Calling the function
+// greet();  
+// greet(); 
+// greet(); 
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// console.log(add(3,4));
+// console.log(add(34,4));
+
+// function multiply(a, b) {
+//     return a * b;
+// }
+
+// console.log(multiply(3,4));
+// console.log(multiply(34,4));
+
+// const square = (n) => n * n;
+// console.log(square(4));  // Output: 16
+
+// const add = (a, b) => a + b;
+// console.log(add(3, 4));
+
+// function greet(name = "Guest") {
+//     console.log("Hello," + name + "!");
+// }
+
+// greet("Krishnansh");
+// greet();
+
+// function power(base, exponent = 2) {
+//     return Math.pow(base, exponent);
+//   }
+
+// console.log(power(3));
+// console.log(power(3, 3));
+// console.log(power(5, 2));
+
+// let fruits = ["apple", "banana", "orange"];
+// fruits.push("mango");
+// fruits.length;
+// fruits.shift()
+// fruits.unshift("Berry")
+// console.log(fruits);
+// // console.log(fruits[0]);
+// console.log(fruits.length);
+
+
+// let person = {
+//     name: "Krishnansh",
+//     age: 22,
+//     city: "Jaipur"
+// };
+// person.country= "India";
+// console.log(person.name);
+// console.log(person["age"]);
+// console.log(person);
+
+
+// let student = {
+//     name: "Arin",
+//     marks: 90,
+//     getDetails: function() {
+//         return this.name + " scored " + this.marks + " marks," + "Great job!";
+//     }
+// };
+
+// console.log(student.getDetails()); 
