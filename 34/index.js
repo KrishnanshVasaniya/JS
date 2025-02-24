a3b05b8154742acdd309aaef5832e8f7fa18eaad
KrishnanshVasaniya <119565702+KrishnanshVasaniya@users.noreply.github.com>
2025-02-24 20:02:35 +0530

    Add files via upload

---
+++ 34/index.js
@@ -0,0 +1,40 @@
+// console.log("Hey I am Learning!");
+
+// let a = 1;
+/* console.log(a);
+console.log(a+1);
+console.log(a+2); */
+// for (let i = 0; i < 100; i++) {
+//     console.log(a+i);
+// }
+
+// let obj = {
+//     name : "Krishnansh",
+//     role : "Programmer",
+//     company : "Google"
+// }
+// for (const key in obj) { 
+//         const element = obj[key];
+//         console.log(key, element)  
+// }
+// for (const c of "Krishnansh") {
+//     console.log(c)
+// }
+
+// let i=0;
+// while(i<6){
+//     i++;
+//     console.log(i);
+// }
+
+// let i = 0;
+// while (i < 6) {
+//     console.log(i);
+//     i++;
+// }
+
+let i=10;
+do {
+    console.log(i)
+    i++;
+} while (i<6);
