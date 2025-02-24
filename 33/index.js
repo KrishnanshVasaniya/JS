e14967da71cebbced7dba7f297a0ed5849759f24
KrishnanshVasaniya <119565702+KrishnanshVasaniya@users.noreply.github.com>
2025-02-24 20:01:48 +0530

    Add files via upload

---
+++ 33/index.js
@@ -0,0 +1,28 @@
+console.log("Hello I am learning this today!!")
+
+let age = 18;
+// let grace = 2;
+
+// age += grace;
+
+/* console.log(age);
+console.log(age + grace);
+console.log(age - grace);
+console.log(age * grace);
+console.log(age / grace);
+console.log(age ** grace);
+console.log(age % grace); */
+
+if(age == 18){
+    console.log("You can Drive");
+}
+if(age != 18){
+    console.log("You can Drive");
+}
+// if((age)>18){
+//     console.log("You can Drive");
+// }
+
+else{
+    console.log("You cannot Drive");
+}
